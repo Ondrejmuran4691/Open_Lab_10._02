eb0ac7d1d5c0f06c8af8f40a41ae355e3b1f7cea
Ondrejmuran4691 <onko.muran@gmail.com>
2021-11-26 08:04:44 +0100

    Add project files.

---
+++ Open_Lab_10._02/Book.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._02
+{
+    class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private string releaseDate;
+        public string _title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = title;
+            }
+        }
+        public int _pages
+           {
+            get
+            {
+                return _pages;
+            }
+    set
+            {
+                _pages = pages;
+            }
+        }
+        public string _category
+{
+    get
+            {
+        return _category;
+    }
+    set
+            {
+        _category = category;
+    }
+}
+public string _author
+{
+    get
+            {
+        return _author;
+    }
+    set
+            {
+        _author = author;
+    }
+}
+public string _releaseDate
+{
+    get
+            {
+        return _releaseDate;
+    }
+    set
+            {
+        _releaseDate = releaseDate;
+    }
+}
+
+
+    }
+}
---
+++ Open_Lab_10._02/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR._title = "LOTR";
+            LOTR._author = "J. R. R. Tolkien";
+            LOTR._category = "Fantasy";
+            LOTR._pages = 9250;
+            LOTR._releaseDate = "29 July 1954";
+            
+
+
+            Console.WriteLine(LOTR._title);
+
+        }
+    }
+}
