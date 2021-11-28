e0a9fa86eac11466242d86a797f6c90b0c6d0aae
Ondrejmuran4691 <onko.muran@gmail.com>
2021-11-28 19:17:39 +0100

    create book LOTR with private variables

--- Open_Lab_10._02/Program.cs
+++ Open_Lab_10._02/Program.cs
@@ -12,7 +12,20 @@ namespace Open_Lab_10._02
     {
         static void Main(string[] args)
         {
-            
+            Book LOTR = new Book();
+            LOTR.atitle = "LOTR";
+            LOTR.aauthor = "J. R. R. Tolkien";
+            LOTR.acategory = "Fantasy";
+            LOTR.apages = 9250;
+            LOTR.areleaseDate = 1954;
+            LOTR.itle();
+            LOTR.uthor();
+            LOTR.ategory();
+            LOTR.ages();
+            LOTR.eleaseDate();
+
+            LOTR.writeall();
+
 
 
 
