7b03678dfec71e2ff2c7cd3f56656b90a95eaa51
Ondrejmuran4691 <onko.muran@gmail.com>
2021-11-28 12:58:31 +0100

    create a object book and variables and write it to console

--- Open_Lab_10._02/Program.cs
+++ Open_Lab_10._02/Program.cs
@@ -12,8 +12,15 @@ namespace Open_Lab_10._02
     {
         static void Main(string[] args)
         {
-            
-         
+            Book LOTR = new Book();
+            LOTR.atitle = "LOTR";
+            LOTR.aauthor = "J. R. R. Tolkien";
+            LOTR.acategory = "Fantasy";
+            LOTR.apages = 9250;
+            LOTR.areleaseDate = "29 July 1954";
+
+            LOTR.writeall();
+
 
         }
     }
